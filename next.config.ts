4a53a4e7fc3c6608e88eebe7db0ef385af0f4110
subhan7676 <subhanfreefire76@gmail.com>
2026-02-18 19:20:11 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -12,6 +13,13 @@ const nextConfig: NextConfig = {
         hostname: "**.googleusercontent.com",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
